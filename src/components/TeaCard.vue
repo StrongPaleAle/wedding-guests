a5b0bd758aba3b887a87f7eb7930cdfb42af4876
StrongAle <alessio.falchi90@gmail.com>
2024-05-18 13:57:18 +0200

    tea pages

--- src/components/TeaCard.vue
+++ src/components/TeaCard.vue
@@ -18,18 +18,51 @@ defineProps<{
 }>()
 </script>
 <template>
-    <div class="card">
+    <div class="card tea-card">
         <img
             v-if="teaCardProps.imageUrl"
             :src="teaCardProps.imageUrl"
             :alt="teaCardProps.imageAlt"
         />
         <div class="card__content">
-            <h2>{{ teaCardProps.title }}</h2>
-            <a :href="teaCardProps.companyLink">{{ t('from') }}: {{ teaCardProps.company }}</a>
-            <p>{{ teaCardProps.description }}</p>
-            <p>{{ t('ingredients') }}: {{ teaCardProps.ingredients }}</p>
-            <p>{{ t('instructions') }}: {{ teaCardProps.instructions }}</p>
+            <div>
+                <h2>{{ teaCardProps.title }}</h2>
+                <p>
+                    <span class="from-label">{{ t('from') }}: </span>
+                    <a :href="teaCardProps.companyLink"> {{ teaCardProps.company }}</a>
+                </p>
+            </div>
+            <p class="italic text-base">{{ teaCardProps.description }}</p>
+            <p>
+                <span>{{ t('ingredients') }}: </span> {{ teaCardProps.ingredients }}
+            </p>
+            <p>
+                <span>{{ t('instructions') }}: </span> {{ teaCardProps.instructions }}
+            </p>
         </div>
     </div>
 </template>
+<style scoped lang="postcss">
+.tea-card {
+    @apply flex flex-col sm:flex-row sm:odd:flex-row-reverse sm:odd:text-right items-start md:items-center p-4 sm:p-6 gap-2 md:gap-6;
+
+    img {
+        @apply w-full sm:max-w-56 md:max-w-xs lg:max-w-md rounded-lg;
+    }
+    .card__content {
+        @apply flex flex-col gap-2 md:gap-4 max-w-prose text-sm flex-grow;
+        span {
+            @apply font-bold text-copper-200 text-base block;
+            &.from-label {
+                @apply inline;
+            }
+        }
+        a {
+            @apply hover:text-copper-300 hover:underline;
+        }
+    }
+    h2 {
+        @apply text-2xl sm:text-3xl md:text-4xl font-serif text-copper-300;
+    }
+}
+</style>
--- src/views/TeaView.vue
+++ src/views/TeaView.vue
@@ -16,15 +16,27 @@ const cherryBlossomTea = {
     imageUrl: '/assets/images/cherry-blossom-tea.jpg',
     imageAlt: ''
 }
+const lovePotionTea = {
+    title: t('LPtitle'),
+    company: t('LPcompany'),
+    companyLink: t('LPcompanyLink'),
+    description: t('LPdescription'),
+    ingredients: t('LPingredients'),
+    instructions: t('LPinstructions'),
+    imageUrl: '/assets/images/love-potion-tea.jpg',
+    imageAlt: ''
+}
 </script>
 <template>
     <div>
         <HomeHero />
         <h2
-            class="mt-10 md:mt-20 text-center text-3xl sm:text-5xl md:text-zxl font-serif text-copper-300"
+            class="mt-10 md:mt-20 text-center text-5xl sm:text-6xl md:text-7xl font-serif text-copper-300"
         >
             Grazie!
         </h2>
-        <TeaCard :teaCardProps="cherryBlossomTea" />
+
+        <TeaCard :teaCardProps="cherryBlossomTea" class="mt-6 lg:mt-10" />
+        <TeaCard :teaCardProps="lovePotionTea" class="mt-6 lg:mt-10" />
     </div>
 </template>
